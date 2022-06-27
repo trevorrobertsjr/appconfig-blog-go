2ae6ce4790dd668da1a80162bd5fa5f0a0299cfc
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-06-26 23:18:29 -0400

    main func can query feature flag. must convert to Lambda

---
+++ go.mod
@@ -0,0 +1,13 @@
+module appconfig-blog-go
+
+go 1.18
+
+require (
+	github.com/aws/aws-sdk-go v1.44.42
+	github.com/aws/jsii-runtime-go v1.61.0
+)
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/appconfigdata"
+	"github.com/aws/jsii-runtime-go"
+)
+
+const (
+	ApplicationIdentifier          string = "appconfig-blog-go"
+	ConfigurationProfileIdentifier string = "WhichSide"
+	EnvironmentIdentifier          string = "dev"
+)
+
+func main() {
+	// Specifying the region when creating the AWS Session
+	mySession, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Create a AppConfigData client from the AWS Session.
+	svc := appconfigdata.New(mySession)
+
+	// Retrieve an AppConfig token to then make the request
+	// for the latest version of the feature flag
+	token, err := svc.StartConfigurationSession(&appconfigdata.StartConfigurationSessionInput{
+		ApplicationIdentifier: jsii.String(ApplicationIdentifier),
+		// The configuration profile ID or the configuration profile name.
+		//
+		// ConfigurationProfileIdentifier is a required field
+		ConfigurationProfileIdentifier: jsii.String(ConfigurationProfileIdentifier),
+
+		// The AppConfig environment ID (ex: dev, beta, prod, etc.).
+		EnvironmentIdentifier: jsii.String(EnvironmentIdentifier),
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	result, err := svc.GetLatestConfiguration(&appconfigdata.GetLatestConfigurationInput{
+		ConfigurationToken: jsii.String(*token.InitialConfigurationToken),
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	// resultStruct := result.Configuration
+	var results map[string]interface{}
+	json.Unmarshal(result.Configuration, &results)
+	birds := results["allegiance"].(map[string]interface{})
+	// json.Unmarshal(result.Configuration, &resultStruct)
+	fmt.Println(birds["choice"])
+}
