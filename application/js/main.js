7cd3ed87313681f8394849400ef7d5ef98bc7c28
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-07-01 09:48:18 -0400

    reorganize dirs. begin sample application

---
+++ application/js/main.js
@@ -0,0 +1,5 @@
+fetch('https://xe5phfcmjxvg7fdy4nqvrop62m0nvdgm.lambda-url.us-east-1.on.aws/')
+    .then(response => response.text())
+    .then((data) => {
+        document.getElementById("displayCecil").innerHTML = data;
+    });
