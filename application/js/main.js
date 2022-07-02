9d68c5d021c2d0b662e764cdbf9c19113a6a8b2c
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-07-01 20:09:13 -0400

    html still in progress

--- application/js/main.js
+++ application/js/main.js
@@ -2,4 +2,5 @@ fetch('https://xe5phfcmjxvg7fdy4nqvrop62m0nvdgm.lambda-url.us-east-1.on.aws/')
     .then(response => response.text())
     .then((data) => {
         document.getElementById("displayCecil").innerHTML = data;
+        console.log(data);
     });
