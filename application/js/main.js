dae7a490e62e4f7af529adbcd9ff6b8df61aeedd
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2023-03-29 19:41:52 -0400

    updated js to accommodate AppConfig JSON response

--- application/js/main.js
+++ application/js/main.js
@@ -1,6 +1,17 @@
-fetch('https://xe5phfcmjxvg7fdy4nqvrop62m0nvdgm.lambda-url.us-east-1.on.aws/')
-    .then(response => response.text())
-    .then((data) => {
-        document.getElementById("displayCecil").innerHTML = data;
-        console.log(data);
-    });
+const htmlDarkKnightOutput =
+  '<section><img src="assets/img/dark_knight_cecil.png" alt="image of Cecil as a Dark Knight"></section><section><p>Cecil continued to walk the path of the Dark Knight. The Path of the Paladin was too new and unfamiliar to Cecil for him to accept. Cecil determined he was strong enough to defeat the King of Baron with the Dark Sword. The spirit of the father of Cecil was disappointed, but he ultimately understood.</p></section>';
+const htmlPaladinOutput =
+  '<section><img src="assets/img/paladin_cecil.png" alt="image of Cecil as a Paladin"></section><section><p>Cecil heeded the words of the light and took his rightful place as the prophesied Paladin. Contrary to his fears, selecting the light grew his power exponentially. He then realized the King of Baron encouraged Cecil to take the dark sword to prevent him from realizing his full potential.</p></section>';
+
+fetch(
+  "http://localhost:2772/applications/blogAppConfigGo/environments/prod/configurations/whichSide?flag=allegiance"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    if ((data.choice = "paladin")) {
+      document.getElementById("displayCecil").innerHTML = htmlPaladinOutput;
+    } else {
+      document.getElementById("displayCecil").innerHTML = htmlDarkKnightOutput;
+    }
+    console.log(data);
+  });
